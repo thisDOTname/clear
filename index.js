5eb7749bb7beb7c931996602c5217271a03bcea1
thisDOTname <hemantrai1988@gmail.com>
2023-07-27 17:07:21 +0530

    Updated channel id

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { App } = require("@slack/bolt");
 
 
 const app = new App({
-    token: "xoxp-3172215028145-3161931853348-3849362926099-76960fa7a3f2d339e371e30fdb6bf6b9", //Find in the Oauth  & Permissions tab
+    token: "xoxp-3172215028145-3161931853348-4008579543329-0b78a347e03bf98161b35d16bb349a70", //Find in the Oauth  & Permissions tab
     signingSecret: "3f8391654e2e054ff2a4ff020f3bbe6e", // Find in Basic Information Tab
     socketMode:true,
-    appToken: "xapp-1-A03QW37RM6Y-3837075872550-d655e7e0a2b2aef36b143d5a86f6c493fcb5835501b9e5d0ffbae8f9cc4518c4" // Token from the App-level Token that we created
+    appToken: "xapp-1-A03QW37RM6Y-3995963138946-3c07707a6a397c7fccf1eb286290b8557b84485181116fce8f7f0c1f00f891cc" // Token from the App-level Token that we created
 });
 
 (async () => {
@@ -20,7 +20,7 @@ app.command("/clean", async ({ command, ack, say, client }) => {
       // Store conversation history
         let conversationHistory;
         // ID of channel you watch to fetch the history for
-        let channelId = "C034GUBABL6";
+        let channelId = "C05JLNGSW15";
 
         try {
         // Call the conversations.history method using WebClient
